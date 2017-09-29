5a3a4c7123a3224c70ef29d147b93906961675d2
joseSantorcuato <santorcuato76@gmail.com>
2017-09-29 13:21:31 -0300

    Se agrega sesión 7.Captura sonido, movimiento y colisión

---
+++ sesion7/18_mov/sketch.js
@@ -0,0 +1,26 @@
+/*
+Taller audiovisual y multimedia II
+Jose Santorcuato
+*/
+var x = 240;
+var speed = 2;
+
+function setup() {
+  createCanvas(240,120);
+  ellipseMode(RADIUS);
+
+  // Create an Audio input
+
+
+}
+
+function draw() {
+  background(0);
+  x -= speed;
+rect(x,20,10,10);
+
+if(x < 0){
+
+x = 240
+}
+}
